b6be5bbfd65e6f700151e5860ea7d4aca3b8b92d
Ahliman <ahliman.huseynov@outlook.com>
2025-01-27 13:09:25 +0000

    Add localized pathnames

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,10 +1,19 @@
-import {createNavigation} from 'next-intl/navigation';
-import {defineRouting} from 'next-intl/routing';
+import { createNavigation } from "next-intl/navigation";
+import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ['en'],
-  defaultLocale: 'en'
+  locales: ["en", "fr"],
+  defaultLocale: "en",
+  pathnames: {
+    // If all locales use the same pathname, a single
+    // external path can be used for all locales
+    "/": "/",
+    "/about": {
+      en: "/about",
+      fr: "/a-propos",
+    },
+  },
 });
 
-export const {Link, redirect, usePathname, useRouter} =
+export const { Link, redirect, usePathname, useRouter } =
   createNavigation(routing);
